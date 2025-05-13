8630eff99ea4f9e906f28c61ad04a3b736549a85
Luis Fernando Farji <fernando.farji@gmail.com>
2025-05-13 12:00:09 -0300

    first init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+const initialState = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+function App() {
+  return (
+    <>
+<h1>Google Translate</h1>
+    </>
+  )
+}
+
+export default App
